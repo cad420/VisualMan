93b6b3f4d382b0b80fa1b79757fb711220b2e8ef
ysl <yangshuoliu@sina.com>
2019-06-20 23:10:43 +0800

    add cmd params

--- test/vmlargevolumeraycast_test.cpp
+++ test/vmlargevolumeraycast_test.cpp
@@ -1,12 +1,19 @@
 
 #include "vmglfwwindow.h"
 #include "vm_largevolumeraycast.h"
+#include "cmdline.h"
 
 int main(int argc,char ** argv)
 {
 	using namespace ysl::vm;
 	using namespace ysl::app;
-	VMGLFWWindow window("LVD Renderer", RenderContextFormat(), 1280,960);
+
+	cmdline::parser clp;
+	clp.add<int>("width",'w',"The width of window",false,1024);
+	clp.add<int>("height", 'h', "The height of window", false, 768);
+	clp.parse_check(argc, argv);
+
+	VMGLFWWindow window("LVD Renderer", RenderContextFormat(), clp.get<int>("width"),clp.get<int>("height"));
 	auto app = MakeRef<VM_LargeVolumeRayCast>();
 	window.AddEventListener(app);
 	auto ret = window.Show();
