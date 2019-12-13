68f2282fd97675a3032d6d6b709a6d181b1cf66a
Shuoliu Yang <yangshuoliu@sina.com>
2019-12-13 15:29:59 +0800

    fix

--- tools/block2raw.cpp
+++ tools/block2raw.cpp
@@ -11,7 +11,7 @@ int main( int argc, char **argv )
 	cmdline::parser a;
 	ysl::PluginLoader::GetPluginLoader()->LoadPlugins( "plugins" );
 	a.add<std::string>( "if", 'i', "input file", true );
-	a.add<std::string>( "of", 'o', "output file", false, "a.out" );
+	a.add<std::string>( "of", 'o', "output file", false, "a" );
 	a.add<std::string>( "fmt", 'f', "file format", true, ".lvd" );
 	a.parse_check( argc, argv );
 	const auto key = a.get<std::string>( "fmt" );
@@ -39,8 +39,15 @@ int main( int argc, char **argv )
 
 	const auto ods = reader->GetDataSizeWithoutPadding();  // original data size
 
+	std::stringstream ss;
+	ss << a.get<std::string>( "of" ) << "_" << ods.x << "_" << ods.y << "_" << ods.z << ".raw";
+	std::string out;
+	ss >> out;
+
+	::vm::println( "Data Size:{}", ods );
+
 	try {
-		if ( file->Open( a.get<std::string>( "of" ), ods.Prod(), FileAccess::ReadWrite, MapAccess::ReadWrite ) == false ) {
+		if ( file->Open( out, ods.Prod(), FileAccess::ReadWrite, MapAccess::ReadWrite ) == false ) {
 			::vm::println( "Failed to open file {}", a.get<std::string>( "of" ) );
 		}
 	} catch ( std::exception &e ) {
@@ -51,31 +58,35 @@ int main( int argc, char **argv )
 	if ( data == nullptr ) {
 		::vm::println( "Failed to map file" );
 	}
-	const auto p = reader->Get3DPageSizeInLog();	   // padding
-	const auto ps = reader->Get3DPageSize();		   // page size
-	const auto _ = ps - 2 * ysl::Size3( p, p, p );
-	const ysl::Vec3i bsnp = ysl::Vec3i(_.x,_.y,_.z);  // block size without padding
+	const auto p = reader->GetPadding();	  // padding
+	const auto bs = reader->Get3DPageSize();  // page size
+	const auto _ = bs - 2 * ysl::Size3( p, p, p );
+	const ysl::Vec3i bsnp = ysl::Vec3i( _.x, _.y, _.z );  // block size without padding
 
-
-	::vm::println( "Padding:{}", p );
-	::vm::println( "PageSize:{}", ps );
+	::vm::println( "PageSize: {}", bs );
+	::vm::println( "PageSizeNoPadding: {}", bsnp );
 
 	for ( int z = 0; z < ods.z; z++ ) {
-		const auto zb = ysl::RoundUpDivide( z, bsnp.z );  // block x id
+		const auto zb = z/ bsnp.z ;  // block x id
 		for ( int y = 0; y < ods.y; y++ ) {				  // block y id
-			const auto yb = ysl::RoundUpDivide( y, bsnp.y );
-			for ( int x = 0; y < ods.x; x++ ) {
-				const auto xb = ysl::RoundUpDivide( x, bsnp.x );																  // block z id
-				const auto bid = ysl::Linear( { int( xb ), int( yb ), int( zb ) }, { pageCount.x,  pageCount.y} );  // linear block id
+			const auto yb =  y/ bsnp.y ;
+			for ( int x = 0; x < ods.x; x++ ) {
+				const auto xb =  x/bsnp.x ;
+				
+				const auto bid = ysl::Linear( { int( xb ), int( yb ), int( zb ) }, { pageCount.x, pageCount.y } );  // linear block id
+				//::vm::println( "Block: {} ==> [{}]",ysl::Size3{xb,yb,zb},bid  );
 				const auto block = reader->GetPage( bid );
 				const auto globalOffset = ysl::Linear( { x, y, z }, { ods.x, ods.y } );
 				assert( globalOffset < ods.Prod() );
-				const auto blockOffset = ysl::Linear( { x % bsnp.x + p, y & bsnp.y + p, z % bsnp.z + p }, { ps.x, ps.y } );
-				assert( blockOffset < ps.Prod() );
-
+				const ysl::Point3i pos = { x % bsnp.x + p, y % bsnp.y + p, z % bsnp.z + p };
+				//::vm::println( "{},{},{},{},{},{},{},{}",y,globalOffset, ysl::Vec3i( xb, yb, zb ),bid, pos,x,y,z );
+				const auto blockOffset = ysl::Linear( pos, { bs.x, bs.y } );
+				assert( blockOffset < bs.Prod() );
 				*( reinterpret_cast<char *>( data ) + globalOffset ) = *( reinterpret_cast<const char *>( block ) + blockOffset );
+				//std::cout << globalOffset << " " << blockOffset << std::endl;
 			}
 		}
 	}
+	file->Close();
 	return 0;
 }
